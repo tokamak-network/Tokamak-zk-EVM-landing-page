785b7e12760f781a8b1c5e8c47e98f7af5d80e83
amrtokmak <aamir@tokamak.network>
2025-10-28 13:04:53 +0800

    feat: prod deployment here

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -60,6 +60,7 @@ export default async function BlogPage() {
           </p>
         </div>
 
+
         {/* Blog Posts Grid */}
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-20">
           {posts.length === 0 ? (
