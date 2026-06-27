0940cde20e079147789dc01e5b5cfe93595c55d5
Jehyuk <cjhyuck213@gmail.com>
2026-06-27 12:24:48 +0800

    Use transparent Tonigma letter logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                 height={571}
                 priority
                 sizes="(max-width: 760px) calc(100vw - 32px), 620px"
-                src="/brand/tonigma-letter-logo.png"
+                src="/brand/tonigma-letter-logo-transparent.png"
                 width={2757}
               />
             </h1>
